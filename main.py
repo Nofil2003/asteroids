952cb3bf638666c3a3f4016d7854be9d031adb7e
Nofil Ali Khan <nofilalikhan@gmail.com>
2026-09-19 12:37:42 +0500

    chore: FPS setting

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from logger import log_state
 def main() -> None:
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(
         f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}"
@@ -20,6 +23,7 @@ def main() -> None:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
