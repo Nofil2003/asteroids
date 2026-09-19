bdd53a5764384b4d6d8db3ab730494b2aee88be6
Nofil Ali Khan <nofilalikhan@gmail.com>
2026-09-19 17:41:05 +0500

    feat: added player movement

--- player.py
+++ player.py
@@ -32,3 +32,15 @@ class Player(circleshape.CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt * -1)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * constants.PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
