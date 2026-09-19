a15e74ee3fc475b3a33d7a054071c499c10a78f6
Nofil Ali Khan <nofilalikhan@gmail.com>
2026-09-19 11:19:40 +0500

    feat: logger + constants

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import constants
+
+
+def main() -> None:
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(
+        f"Screen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}"
+    )
+
+
+if __name__ == "__main__":
+    main()
