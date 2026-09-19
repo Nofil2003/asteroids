92da9aca073b41f434469f24bf82091eed1a4371
Nofil Ali Khan <nofilalikhan@gmail.com>
2026-09-19 19:46:46 +0500

    refactor: using groups like updatable and redrawable instead of a single player object

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main() -> None:
     clock = pygame.time.Clock()
     dt = 0.0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player1 = Player(
         constants.SCREEN_WIDTH / 2,
         constants.SCREEN_HEIGHT / 2,
@@ -29,8 +33,9 @@ def main() -> None:
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player1.update(dt)
-        player1.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
